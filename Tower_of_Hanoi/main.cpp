bf0d14fb3938649a40808fd443657f4b0a56e8cd
shin <79005737+Shinhisu@users.noreply.github.com>
2024-03-26 16:40:00 +0900

    하노이 탑 과제 초기 코드

---
+++ Tower_of_Hanoi/main.cpp
@@ -0,0 +1,17 @@
+#include "std_lib_facilities.h"
+
+int main()
+try {
+    
+    cout << "dd";
+    keep_window_open();
+    return 0;
+}
+catch (runtime_error& e) {
+    cout << "runtime_error : " << e.what() << "\n";
+    keep_window_open();
+}
+catch (...) {
+    cout << "something error" << "\n";
+    keep_window_open();
+}
